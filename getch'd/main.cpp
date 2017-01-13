fb4bddea5dd5f1c2573280f191bc754fa5011f0f
alamasanute <almmondducherr@yahoo.com>
2017-01-13 20:29:13 +0200

    Add files via upload

---
+++ getch'd/main.cpp
@@ -0,0 +1,14 @@
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     char key = ' ';
+     cout << "Enter a key" << endl;
+     key = _getch();
+     cout << "You entered: " << key << endl;
+
+    return 0;
+
+}
